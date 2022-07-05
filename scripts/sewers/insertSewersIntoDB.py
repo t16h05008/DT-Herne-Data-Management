48a34e16e73dccac7cada2b9ec0b983804d9a3bf
T. Herker <tim.herker@stud.hs-bochum.de>
2022-07-05 05:33:30 +0200

    Remove import of bbox file since it doesn't exist anymore

--- scripts/sewers/insertSewersIntoDB.py
+++ scripts/sewers/insertSewersIntoDB.py
@@ -26,24 +26,18 @@ def main():
     
      # get path for each file that should be processed
     files_to_process = glob.glob(os.path.join(input_path, '*.geojson'))
-    files_to_process.extend(glob.glob(os.path.join(input_path, '*.json')))
 
     for file_path in files_to_process:
         filename = file_path.split("\\")[-1]
         collection_suffix = ""
-        is_bbox_file = False
         if filename == "shafts.geojson" :
             collection_suffix = "shafts"
         elif filename == "pipes.geojson":
             collection_suffix = "pipes"
-        elif filename == "pipes.bboxInfo.json":
-            collection_suffix = "pipes.bboxInfo"
-            is_bbox_file = True
         else:
             raise Exception("Filename must be \
-                'shafts.geojson', \
-                'pipes.geojson', or \
-                'pipes.bboxInfo.json' (to determine the collection to insert into)")
+                'shafts.geojson' or \
+                'pipes.geojson' (to determine the collection to insert into)")
 
         collection_name = config["collection"] + "." + collection_suffix
         # drop collection id it existed
@@ -52,44 +46,31 @@ def main():
         
         collection = db[collection_name]
 
-        # Do different things depending on if we have a bboxInfo file or the real geojson
-        if(is_bbox_file):
-            # Insert as normal document
-            file_content = None
-            with open(file_path,'r') as f:
-                file_content = json.loads(f.read())
-                # Add a reference to the original collection
-                file_content["collectionName"] = collection.name.replace(".bboxInfo", "")
-                # Insert into db
-                result = collection.insert_one(file_content)
-                print("Inserted bbox info for", file_content["collectionName"])
-        else:
-            # insert as geojson
-            geojson = None
-            with open(file_path,'r') as f:
-                geojson = json.loads(f.read())
+        geojson = None
+        with open(file_path,'r') as f:
+            geojson = json.loads(f.read())
 
-            # create 2dsphere index
-                collection.create_index([("geometry", GEOSPHERE)])
+        # create 2dsphere index
+            collection.create_index([("geometry", GEOSPHERE)])
 
-            bulk_arr = []
-            for feature in geojson['features']:
-                bulk_arr.append( InsertOne(feature) )
+        bulk_arr = []
+        for feature in geojson['features']:
+            bulk_arr.append( InsertOne(feature) )
 
-            # execute bulk operation to the DB
-            try:
-                result = collection.bulk_write(bulk_arr)
-                print("Collection:", collection.name, ": Number of Features successfully inserted:", result.inserted_count)
-            except BulkWriteError as bwe:
-                nInserted = bwe.details["nInserted"]
-                errMsg = bwe.details["writeErrors"]
-                print("Errors encountered inserting features")
-                print("Number of Features successfully inserted:", nInserted)
-                print("The following errors were found:")
-                for item in errMsg:
-                    print("Index of feature:", item["index"])
-                    print("Error code:", item["code"])
-                    print("Message (truncated due to data length):", item["errmsg"][0:120], "...")
+        # execute bulk operation to the DB
+        try:
+            result = collection.bulk_write(bulk_arr)
+            print("Collection:", collection.name, ": Number of Features successfully inserted:", result.inserted_count)
+        except BulkWriteError as bwe:
+            nInserted = bwe.details["nInserted"]
+            errMsg = bwe.details["writeErrors"]
+            print("Errors encountered inserting features")
+            print("Number of Features successfully inserted:", nInserted)
+            print("The following errors were found:")
+            for item in errMsg:
+                print("Index of feature:", item["index"])
+                print("Error code:", item["code"])
+                print("Message (truncated due to data length):", item["errmsg"][0:120], "...")
 
 
 def readConfig():
