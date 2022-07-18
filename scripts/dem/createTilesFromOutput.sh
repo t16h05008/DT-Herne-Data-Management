bf819914d07e54084a73c6e2c289b6d6d32b650e
T. Herker <tim.herker@stud.hs-bochum.de>
2022-07-18 21:47:34 +0200

    some formatting

--- scripts/dem/createTilesFromOutput.sh
+++ scripts/dem/createTilesFromOutput.sh
@@ -3,13 +3,11 @@ gdalbuildvrt ./output/dem50.vrt ./output/dem50/*.tiff
 gdalbuildvrt ./output/dem25.vrt ./output/dem25/*.tiff
 gdalbuildvrt ./output/dem10.vrt ./output/dem10/*.tiff
 gdalbuildvrt ./output/dem1.vrt ./output/dem1/*.tiff
-
 echo "Creating subdirectories for each resolution"
 mkdir ./output/terrain50
 mkdir ./output/terrain25
 mkdir ./output/terrain10
 mkdir ./output/terrain1
-
 echo "Creating tiles for resolution 50"
 ctb-tile -f Mesh -C -N -o ./output/terrain50 ./output/dem50.vrt
 ctb-tile -f Mesh -C -N -l -o ./output/terrain50 ./output/dem50.vrt
@@ -22,4 +20,3 @@ ctb-tile -f Mesh -C -N -l -o ./output/terrain10 ./output/dem10.vrt
 echo "Creating tiles for resolution 1"
 ctb-tile -f Mesh -C -N -o ./output/terrain1 ./output/dem1.vrt
 ctb-tile -f Mesh -C -N -l -o ./output/terrain1 ./output/dem1.vrt
-
